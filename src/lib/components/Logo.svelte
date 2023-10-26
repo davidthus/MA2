1e4bf4c00956a26103be2626283044f8b74cf1bd
davidthus <dvdnicorici05@gmail.com>
2023-10-26 23:01:36 +0100

    add font

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,4 +1,4 @@
 <a class="flex flex-col bg-transparent w-56 border-gray-100 border p-3" href="/">
-	<h1 class="text-slate-50 text-3xl font-mono text-left min-w-full">Mathical</h1>
-	<h1 class="text-slate-50 text-3xl font-mono text-right min-w-full">Adventures</h1>
+	<h1 class="text-slate-50 font-kay text-3xl font-mono text-left min-w-full">Mathical</h1>
+	<h1 class="text-slate-50 font-kay text-3xl font-mono text-right min-w-full">Adventures</h1>
 </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { auth } from '$lib/firebase';
-	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+	import { auth, user } from '$lib/firebase';
+	import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 	async function signInWithGoogle() {
 		const provider = new GoogleAuthProvider();
@@ -10,10 +10,21 @@
 	}
 </script>
 
-<p class="text-8xl text-slate-50 w-9/12 mt-6 text-center">a character is in need of you...</p>
-<div class="mt-6">
+{#if $user}
+	<h2 class="text-lg text-slate-50 mt-6 font-playpen">Welcome, {$user.displayName}</h2>
+	<p class="text-center text-green-500 font-playpen">You are logged in</p>
 	<button
-		class="py-4 px-8 mt-8 rounded-full text-slate-50 bg-blue-600 outline-none"
-		on:click={signInWithGoogle}>Make Myself Known</button
+		class="py-4 px-8 mt-8 rounded-full text-slate-50 bg-blue-600 outline-none font-playpen"
+		on:click={async () => {
+			await signOut(auth);
+		}}>Sign out</button
 	>
-</div>
+{:else}
+	<p class="text-8xl text-slate-50 w-9/12 mt-6 text-center">a character is in need of you...</p>
+	<div class="mt-6">
+		<button
+			class="py-4 px-8 mt-8 rounded-full text-slate-50 bg-blue-600 outline-none"
+			on:click={signInWithGoogle}>MAKE MYSELF KNOWN</button
+		>
+	</div>
+{/if}
