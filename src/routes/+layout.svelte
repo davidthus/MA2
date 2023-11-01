780faae2f56829710164c99f37acce47b38eb5fa
davidthus <dvdnicorici05@gmail.com>
2023-11-01 21:28:14 +0000

    remove unecessary import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	import Logo from '$lib/components/Logo.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import { user, userData } from '$lib/firebase';
 	import '../app.css';
 
 	$user;
 	$userData;
+
+	console.log($user, $userData);
 </script>
 
 <div class="min-h-screen min-w-screen flex flex-col bg-slate-900 items-center p-6">
