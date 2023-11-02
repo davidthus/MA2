18fef9a737296b6a4cf403da053396817d7af6a1
davidthus <dvdnicorici05@gmail.com>
2023-11-02 20:46:27 +0000

    create userData interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,18 @@ export interface IAct {
 	description: string;
 	chapters: { number: number; title: string }[];
 }
+
+export interface UserData {
+	email?: string;
+	uid?: string;
+	displayName?: string;
+	photoURL?: string;
+	joined?: number;
+	stripeCustomerId?: string;
+	is_pro?: boolean;
+	expires?: number;
+	pro_status?: 'lifetime' | 'active' | 'past_due' | 'expiring' | 'canceled';
+	acts?: {
+		[key: string]: string;
+	};
+}
