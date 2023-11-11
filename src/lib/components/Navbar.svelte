9e1a7b7ae93f2f5ff98433b041421f148f208763
davidthus <dvdnicorici05@gmail.com>
2023-11-11 23:47:54 +0000

    enhance pro anchor styles

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,7 +8,10 @@
 	<nav class="flex gap-8">
 		<ul class="flex">
 			<li class="p-4 border-gray-50 text-gray-50 font-kay text-lg">
-				<a href="/pro">Pro</a>
+				<a
+					class="transition-shadow duration-300 text-base font-normal text-green-500 border-green-400 border rounded-md px-2 py-1 hover:drop-shadow-[0_0_5px_rgba(34,197,94,0.9)]"
+					href="/pro">PRO</a
+				>
 			</li>
 			<li class="p-4 border-gray-50 text-gray-50 font-kay text-lg">
 				<a href="/acts">Acts</a>
